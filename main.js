124f9e72b7ce835a0169474043c4d1f187b10f5d
YuyaIsh <dame.blazer@gmail.com>
2023-06-21 22:12:50 +0900

    動画付きのスポンサー商品も非表示

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-var ads = document.querySelectorAll(".AdHolder");
+var spans = document.querySelectorAll('span'); // 全ての <span> 要素を取得
 
-ads.forEach(function( value ) {
-    value.style.display = "none";
+Array.from(spans).forEach(function(span) {
+    if (span.textContent.includes('スポンサー')) {  // スポンサーと記載されているものを取得
+        var parentDiv = span.closest('.s-result-item');  // 親要素で商品を取得
+        if (parentDiv) {
+            parentDiv.style.display = "none";  // 非表示
+        }
+    }
 });
