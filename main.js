ff4475b1a3e29d3c7d34d2bfbaf27852993f4995
YuyaIsh <dame.blazer@gmail.com>
2023-06-21 21:29:53 +0900

    スポンサー表示商品を非表示するよう修正 一部非対応

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-document.querySelector(".AdHolder").style.display = "none";
+var ads = document.querySelectorAll(".AdHolder");
+
+ads.forEach(function( value ) {
+    value.style.display = "none";
+});
